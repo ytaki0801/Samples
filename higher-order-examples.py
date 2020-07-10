16395e1aa642d95853d00d8be108a23887b760e5
TAKIZAWA Yozo <y@nbk.bz>
2020-07-10 21:34:40 +0900

    Some codes are added

--- higher-order-examples.py
+++ higher-order-examples.py
@@ -26,3 +26,24 @@ def f(x, y):
     return(2 * x + y)
 
 f(10, 5) # =>  115
+
+def args_10_5(f):
+    return (lambda : f(10, 5))
+
+args_10_5(f)() # => f(10, 5)
+
+def f(x):
+    return (lambda y: lambda z: lambda w: 4 * x + 3 * y + 2 * z + w)
+
+f(2)(3)(4)(5) # => 4 * 2 + 3 * 3 + 2 * 4 + 5 = 30
+
+def unfold(pred, f, update, seed):
+    if pred(seed):
+        return ([])
+    else:
+        r = unfold(pred, f, update, update(seed))
+        r.insert(0, f(seed))
+        return (r)
+
+unfold(lambda x: x > 10, lambda x: x * x, lambda x: x + 1, 1)
+# => [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
