675d2f2b9e991df36b37c8df490d41bdbd53c681
TAKIZAWA Yozo <y@nbk.bz>
2020-07-11 14:39:54 +0900

    Modified for decorators

--- higher-order-examples.py
+++ higher-order-examples.py
@@ -1,12 +1,11 @@
+# 
+# Basics and Decorators
+#
+
 def threetimes(f):
-    return (lambda x, y: f(f(f(x, y), y), y))
-
-def f(x, y):
-    return (x + y)
-
-threetimes(f)(10, 5)
-# => f(f(f(10, 5), 5), 5)
-# => (((10 + 5) + 5) + 5) = 25
+    def retfunc(x, y):
+        return (f(f(f(x, y), y), y))
+    return (retfunc)
 
 def f(x, y):
     return (2 * x + y)
@@ -17,15 +16,34 @@ threetimes(f)(10, 5)
 
 @threetimes
 def f(x, y):
-    return(x + y)
-
-f(10, 5) # => 25
-
-@threetimes
-def f(x, y):
     return(2 * x + y)
 
 f(10, 5) # =>  115
+
+def threetimes(mes):
+    def _threetimes(f):
+        def retfunc(x, y):
+            print(mes, end="")
+            print(f(f(f(x, y), y), y))
+        return (retfunc)
+    return (_threetimes)
+
+@threetimes(mes = "Result = ")
+def f(x, y):
+    return(2 * x + y)
+
+f(10, 5) # => "Result = 115"
+
+@threetimes(mes = "")
+def f(x, y):
+    return (2 * x + y)
+
+f(10, 5) # => "115"
+
+
+#
+# other stuff from https://ja.wikipedia.org/wiki/%E9%AB%98%E9%9A%8E%E9%96%A2%E6%95%B0
+#
 
 def args_10_5(f):
     return (lambda : f(10, 5))
