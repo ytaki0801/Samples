f589f81d40adf67eb58702534ba228a79b85e5bd
TAKIZAWA Yozo <y@nbk.bz>
2020-07-17 11:18:48 +0100

    Added for fizzbuzz.c

---
+++ FizzBuzz/fizzbuzz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+  for (int n = 1; n <= 100; n++) {
+    if ((n % 3 == 0) && (n % 5 == 0))
+      printf("FizzBuzz ");
+    else if (n % 3 == 0)
+      printf("Fizz ");
+    else if (n % 5 == 0)
+      printf("Buzz ");
+    else
+      printf("%d ", n);
+  }
+  return (0);
+}
