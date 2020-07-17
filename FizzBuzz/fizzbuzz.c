4959b393e854291a1e883edeb8150b7e839915c5
TAKIZAWA Yozo <y@nbk.bz>
2020-07-17 11:56:46 +0100

    Some keywords are modified

--- FizzBuzz/fizzbuzz.c
+++ FizzBuzz/fizzbuzz.c
@@ -2,15 +2,10 @@
 
 int main(void)
 {
-  for (int n = 1; n <= 100; n++) {
-    if ((n % 3 == 0) && (n % 5 == 0))
-      printf("FizzBuzz ");
-    else if (n % 3 == 0)
-      printf("Fizz ");
-    else if (n % 5 == 0)
-      printf("Buzz ");
-    else
-      printf("%d ", n);
-  }
+  for (int n = 1; n <= 100; n++)
+    if (n % 3 == 0 && n % 5 == 0) printf("FizzBuzz ");
+    else if (n % 3 == 0) printf("Fizz ");
+    else if (n % 5 == 0) printf("Buzz ");
+    else printf("%d ", n);
   return (0);
 }
