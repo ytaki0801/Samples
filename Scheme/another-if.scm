a2acf4eebd5b88baba33baca18cd7728d43ed0f3
TAKIZAWA Yozo <y@nbk.bz>
2020-07-23 01:39:30 +0900

    another-if using lambda calculus

--- Scheme/another-if.scm
+++ Scheme/another-if.scm
@@ -1,8 +1,27 @@
-(define wh
-  (lambda (c t f)
-    (cond (c (t)) (else (f)))))
+(define exec1 (lambda (x y) (x)))
+(define exec2  (lambda (x y) (y)))
 
-(define a 20)
-(define b 10)
-(wh (< a b) (lambda () (display "<"))
-            (lambda () (display ">")))
+(exec1 (lambda () (display "exec1" ))
+       (lambda () (display "exec2")))
+; => exec1
+(exec2 (lambda () (display "exec1" ))
+       (lambda () (display "exec2")))
+; => exec2
+
+(define gtn (lambda (a b) (cond ((> a b) exec1) (else exec2))))
+(define ltn (lambda (a b) (cond ((< a b) exec1) (else exec2))))
+(define eqn (lambda (a b) (cond ((= a b) exec1) (else exec2))))
+
+(define a 10)
+(define b 20)
+((gtn a b)
+  (lambda () (display "true" ))
+  (lambda () (display "false")))
+; => false
+
+(define fib
+  (lambda (x f1 f2)
+    ((eqn x 1)
+     (lambda () f1)
+     (lambda () (fib (- x 1) f2 (+ f1 f2))))))
+(display (fib 50 0 1))
