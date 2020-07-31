af1752c1c393d77a3c5baac1a67e7622d6fd195f
TAKIZAWA Yozo <y@nbk.bz>
2020-07-31 21:43:26 +0900

    Added for Z combinator sample on Go lang

---
+++ fixpoint-recur/fixpoint-recur.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+type INT int // int, uint, int64 or uint64 for factorial
+type IR func(IR) func(INT) INT
+
+func main() {
+    // Z combinator
+    var Z = func(f func(func(INT) INT) func(INT) INT) func(INT) INT {
+        return func(g func(IR) func(INT) INT) func(INT) INT {
+            return g(g)
+                }(func(g IR) func(INT) INT {
+                    return f(func(x INT) INT {
+                        return (g(g))(x)
+                    })
+                })
+        }
+        var rz = Z(func(f func(INT) INT) func(INT) INT { return func(n INT) INT { if n == 0 { return 1 } else { return n * f(n-1) } } })(5)
+	fmt.Println(rz)
+}
