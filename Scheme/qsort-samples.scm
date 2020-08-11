190acc0f4d450ce65722dce0856058ad6657c53d
TAKIZAWA Yozo <y@nbk.bz>
2020-08-11 20:30:21 +0900

    Modified for qsort samples on Scheme

--- Scheme/qsort-samples.scm
+++ Scheme/qsort-samples.scm
@@ -1,6 +1,8 @@
 (define D '(14 91 26 58 66 87 25 41 72 50 20 9 27 31 12 28 16 99 100 80))
 
-(use srfi-42)
+;;;; SRFI 42: list-ec
+; gosh -u srfi-42
+; guile --use-srfi=42
 (define qsort1
   (lambda (L)
     (if (null? L) '()
@@ -11,6 +13,9 @@
                (qsort1 (list-ec (: x r) (if (>= x p)) x)))))))
 (display (qsort1 D)) (newline)
 
+;;;; SRFI 1: partition, SRFI 8: receive
+; gosh (no need to import modules)
+; guile --use-srfi=1,8
 (define qsort2
   (lambda (L)
     (if (null? L) '()
