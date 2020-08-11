0c2fa5593457977b09264523bc07f40082cfbe42
TAKIZAWA Yozo <y@nbk.bz>
2020-08-11 23:20:28 +0900

    Modefined for quick sort samples on Python3

--- Python3/qsort-sample.py
+++ Python3/qsort-sample.py
@@ -1,20 +1,26 @@
-D = range(20)
-print(list(D))
+from time import time
 
+D = range(996, -1, -1)
+
+start = time()
 def qsort1(L):
     if not L: return []
     else:
-        p = L[0]
-        LL = qsort1([x for x in L[1:] if x >  p])
-        LR = qsort1([x for x in L[1:] if x <= p])
-        return LL + [p] + LR
-print(list(qsort1(D)))
+        LL = qsort1([x for x in L[1:] if x <  L[0]])
+        LR = qsort1([x for x in L[1:] if x >= L[0]])
+        return LL + [L[0]] + LR
+a = list(qsort1(D))
+print("  comprehension: ", time() - start)
 
+start = time()
 def qsort2(L):
     if not L: return []
     else:
-        p = L[0]
-        LL = qsort2(list(filter(lambda x: x >  p, L[1:])))
-        LR = qsort2(list(filter(lambda x: x <= p, L[1:])))
-        return LL + [p] + LR
-print(list(qsort2(D)))
+        LL = qsort2(list(filter(lambda x: x <  L[0], L[1:])))
+        LR = qsort2(list(filter(lambda x: x >= L[0], L[1:])))
+        return LL + [L[0]] + LR
+a = list(qsort2(D))
+print("filter + lambda: ", time() - start)
+
+#   comprehension:  0.1841585636138916
+# filter + lambda:  0.3261446952819824
