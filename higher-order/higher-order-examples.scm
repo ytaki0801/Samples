5af16ac0c2aaf32267d53e76ee2f83b92e791ff6
TAKIZAWA Yozo <y@nbk.bz>
2020-08-13 07:55:27 +0900

    Typo

--- higher-order/higher-order-examples.scm
+++ higher-order/higher-order-examples.scm
@@ -1,45 +1,52 @@
 (define threetimes
   (lambda (f)
     (lambda (x y)
-      (print (f (f (f x y) y) y)))))
+      (display (f (f (f x y) y) y)))))
 
 (define f (lambda (x y) (+ (* 2 x) y)))
 ((threetimes f) 10 5)
 ; => (f (f (f 10 5) 5) 5)
 ; => (+ (* 2 (+ (* 2 (+ (* 2 10) 5)) 5)) 5) => "115"
+(newline)
 
 (define threetimes_message
   (lambda (f . mes)
     (lambda (x y)
       (if (not (null? mes)) (display (car mes)))
-      (print (f (f (f x y) y) y)))))
+      (display (f (f (f x y) y) y)))))
 
 ((threetimes_message f "Result = ") 10 5)
 ; => "Result = 115"
+(newline)
 ((threetimes_message f) 10 5)
 ; => "115"
+(newline)
 
 
 (define args-10-5 (lambda (f) (f 10 5)))
 (define f
   (lambda (x y)
     (display "x = ") (display x)
-    (display ", y = ") (print y)))
+    (display ", y = ") (display y)))
 (args-10-5 f) ; => "x = 10, y = 5"
+(newline)
 
 (define f (lambda (x y z w) (+ (* 4 x) (* 3 y) (* 2 z) w)))
-(f 2 3 4 5) ; => 30
+(display (f 2 3 4 5)) ; => 30
+(newline)
 
 (define f
   (lambda (x) (lambda (y) (lambda (z) (lambda (w)
     (+ (* 4 x) (* 3 y) (* 2 z) w))))))
-((((f 2) 3) 4) 5) ; => 30
+(display ((((f 2) 3) 4) 5)) ; => 30
+(newline)
 
+(define sqr (lambda (x) (expt x 2)))
 (define unfold
   (lambda (pred f update seed)
     (cond ((pred seed) '())
           (else
            (cons (f seed)
                  (unfold pred f update (update seed)))))))
-(unfold (lambda (x) (> x 10)) square (lambda (x) (+ x 1)) 1)
+(display (unfold (lambda (x) (> x 10)) sqr (lambda (x) (+ x 1)) 1))
 ; => (1 4 9 16 25 36 49 64 81 100)
