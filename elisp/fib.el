ff008e90254e1e3c35b32509d8bcfcb81a1560d0
TAKIZAWA Yozo <y@nbk.bz>
2021-03-23 14:49:39 +0900

    Fixed for the example of iteration

--- elisp/fib.el
+++ elisp/fib.el
@@ -1,5 +1,3 @@
-(setq lexical-binding t)
-
 (defun fib1 (x)
   (cond ((= x 0) 0)
 	((= x 1) 1)
@@ -10,13 +8,13 @@
 (defun fib2 (x f1 f2)
   (if (= x 0) f1
     (fib2 (- x 1) f2 (+ f1 f2))))
-(fib2 500 0 1)
+(fib2 43 0 1)
 
 (defun fib3 (x f1 f2)
-  (while (> x 0)
-    (let ((r f1))
-      (setq x (- x 1))
-      (setq f1 f2)
-      (setq f2 (+ r f2))))
+  (while (>= x 0)
+    (setq r f1
+	  f1 f2
+	  f2 (+ r f2)
+	  x  (- x 1)))
   r)
 (fib3 10 0 1)
