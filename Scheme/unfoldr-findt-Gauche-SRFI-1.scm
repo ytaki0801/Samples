c122e63a009b4a3011e27dd8969c2d0ceb6084ff
TAKIZAWA Yozo <y@nbk.bz>
2021-12-16 20:52:53 +0900

    Fixed filter in unfoldr-findt

--- Scheme/unfoldr-findt-Gauche-SRFI-1.scm
+++ Scheme/unfoldr-findt-Gauche-SRFI-1.scm
@@ -6,32 +6,34 @@
 
 (define (myappend a b)
   (unfold-right null? (^x (car x)) (^x (cdr x)) (myreverse a) b))
-(myappend '(a b c) '(x y z)) ; => (a b c x y z)
+(print (myappend '(a b c) '(x y z))) ; => (a b c x y z)
 
 (define (mymap1 f s)
   (unfold-right null? (^x (f (car x))) (^x (cdr x)) (myreverse s)))
-(mymap1 (^x (+ x 1)) '(10 20 30 40 50)) ; => (11 21 31 41 51)
+(print (mymap1 (^x (+ x 1)) '(10 20 30 40 50))) ; => (11 21 31 41 51)
 
 (define (myrange s e d)
   (myreverse (unfold-right (^x (> x (- e 1))) identity (^x (+ x d)) s)))
-(myrange 11 17 1) ; => (11 12 13 14 15 16)
+(print (myrange 11 17 1)) ; => (11 12 13 14 15 16)
 
 (define (myfind p s)
   (let ((r (find-tail p s)))
     (if r (car r) r)))
-(myfind (^x (eq? x 'c)) '(a b c d e)) ; => c
-(myfind (^x (eq? x 'z)) '(a b c d e)) ; => #f
+(print (myfind (^x (eq? x 'c)) '(a b c d e))) ; => c
+(print (myfind (^x (eq? x 'z)) '(a b c d e))) ; => #f
 
 (define (mymember e s p)
   (find-tail (^x (p x e)) s))
-(mymember 'c '(a b c d e) eq?) ; => (c d e)
-(mymember 'z '(a b c d e) eq?) ; => #f
+(print (mymember 'c '(a b c d e) eq?)) ; => (c d e)
+(print (mymember 'z '(a b c d e) eq?)) ; => #f
 
 (define (myassoc k al p)
   (car (find-tail (^x (p (car x) k)) al)))
-(myassoc 'b '((a . 1) (b . 2) (c . 3) (b . 4) (e . 5)) eq?) ; => (b . 2)
+(print (myassoc 'b '((a . 1) (b . 2) (c . 3) (b . 4) (e . 5)) eq?)) ; => (b . 2)
 
 (define (myfilter p s)
-  (unfold not (^x (car x)) (^x (find-tail p (cdr x))) (find-tail p s)))
-(myfilter even? (myrange 0 10 1)) ; => (0 2 4 6 8)
-(myfilter even? (myrange 1 12 2)) ; => ()
+  (unfold-right not (^x (car x)) (^x (find-tail p (cdr x)))
+                (find-tail p (myreverse s))))
+(print (myfilter even? (myrange 0 10 1))) ; => (0 2 4 6 8)
+(print (myfilter even? (myrange 1 12 2))) ; => ()
+
