0b1740f7c63e1d3d01800f3193476359373cbcee
TAKIZAWA Yozo <y@nbk.bz>
2021-12-17 10:39:12 +0900

    Changed styles for lambda

--- Python3/myfunctools/myfunctools.py
+++ Python3/myfunctools/myfunctools.py
@@ -11,39 +11,39 @@ def find_tail(p, a):
     return r if r else False
 
 def reverse(a):
-    return unfold_right(lambda x: not x,
-                        lambda x: x[0],
-                        lambda x: x[1:],
-                        a)
+    def p(x): return not x
+    def f(x): return x[0]
+    def g(x): return x[1:]
+    return unfold_right(p, f, g, a)
 
 def append(a, b):
-    return unfold_right(lambda x: not x,
-                        lambda x: x[0],
-                        lambda x: x[1:],
-                        reverse(a),
-                        b)
+    def p(x): return not x
+    def f(x): return x[0]
+    def g(x): return x[1:]
+    return unfold_right(p, f, g, reverse(a), b)
 
-def map1(f, s):
-    return unfold_right(lambda x: not x,
-                        lambda x: f(x[0]),
-                        lambda x: x[1:],
-                        reverse(s))
+def map1(f1, s):
+    def p(x): return not x
+    def f(x): return f1(x[0])
+    def g(x): return x[1:]
+    return unfold_right(p, f, g, reverse(s))
 
 def iota(c, *rest):
     if   len(rest) == 0: s, d = 0, 1
     elif len(rest) == 1: s, d = rest[0], 1
     else: s, d = rest[0], rest[1]
-    return reverse(unfold_right(lambda x: x > c - 1,
-                                lambda x: x * d + s,
-                                lambda x: x + 1,
-                                0))
+    def p(x): return x > c - 1
+    def f(x): return x * d + s
+    def g(x): return x + 1
+    return reverse(unfold_right(p, f, g, 0))
 
 def find(p, s):
     r = find_tail(p, s)
     return r[0] if r else r
 
 def member(e, s):
-    return find_tail(lambda x: x == e, s)
+    def p(x): return x == e
+    return find_tail(p, s)
 
 def assoc(k, al):
     return find_tail(lambda x: x[0] == k, al)[0]
